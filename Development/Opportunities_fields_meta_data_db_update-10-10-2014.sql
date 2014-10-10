1c52dcc1d20e82e90372182478883a32d79d9b54
Rick Timmis <rtimmis@wifispark.com>
2014-10-10 11:22:54 +0100

    Turns out that customisation also make changes to the fields_meta_data table in the database. Well of course they would given then we added new fields to the custom opportunities module This commit adds a new SQL file that can be used to update that table on the Production database.

---
+++ Development/Opportunities_fields_meta_data_db_update-10-10-2014.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 10, 2014 at 11:21 AM
+-- Server version: 5.5.38-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `sparkcrm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fields_meta_data`
+--
+
+DROP TABLE IF EXISTS `fields_meta_data`;
+CREATE TABLE IF NOT EXISTS `fields_meta_data` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `vname` varchar(255) DEFAULT NULL,
+  `comments` varchar(255) DEFAULT NULL,
+  `help` varchar(255) DEFAULT NULL,
+  `custom_module` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `len` int(11) DEFAULT NULL,
+  `required` tinyint(1) DEFAULT '0',
+  `default_value` varchar(255) DEFAULT NULL,
+  `date_modified` datetime DEFAULT NULL,
+  `deleted` tinyint(1) DEFAULT '0',
+  `audited` tinyint(1) DEFAULT '0',
+  `massupdate` tinyint(1) DEFAULT '0',
+  `duplicate_merge` smallint(6) DEFAULT '0',
+  `reportable` tinyint(1) DEFAULT '1',
+  `importable` varchar(255) DEFAULT NULL,
+  `ext1` varchar(255) DEFAULT NULL,
+  `ext2` varchar(255) DEFAULT NULL,
+  `ext3` varchar(255) DEFAULT NULL,
+  `ext4` text,
+  PRIMARY KEY (`id`),
+  KEY `idx_meta_id_del` (`id`,`deleted`),
+  KEY `idx_meta_cm_del` (`custom_module`,`deleted`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `fields_meta_data`
+--
+
+INSERT INTO `fields_meta_data` (`id`, `name`, `vname`, `comments`, `help`, `custom_module`, `type`, `len`, `required`, `default_value`, `date_modified`, `deleted`, `audited`, `massupdate`, `duplicate_merge`, `reportable`, `importable`, `ext1`, `ext2`, `ext3`, `ext4`) VALUES
+('Accountsjjwg_maps_address_c', 'jjwg_maps_address_c', 'LBL_JJWG_MAPS_ADDRESS', 'Address', 'Address', 'Accounts', 'varchar', 255, 0, NULL, '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', NULL, '', '', ''),
+('Accountsjjwg_maps_geocode_status_c', 'jjwg_maps_geocode_status_c', 'LBL_JJWG_MAPS_GEOCODE_STATUS', 'Geocode Status', 'Geocode Status', 'Accounts', 'varchar', 255, 0, NULL, '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', NULL, '', '', ''),
+('Accountsjjwg_maps_lat_c', 'jjwg_maps_lat_c', 'LBL_JJWG_MAPS_LAT', '', 'Latitude', 'Accounts', 'float', 10, 0, '0.00000000', '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', '8', '', '', ''),
+('Accountsjjwg_maps_lng_c', 'jjwg_maps_lng_c', 'LBL_JJWG_MAPS_LNG', '', 'Longitude', 'Accounts', 'float', 11, 0, '0.00000000', '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', '8', '', '', ''),
+('Casesjjwg_maps_address_c', 'jjwg_maps_address_c', 'LBL_JJWG_MAPS_ADDRESS', 'Address', 'Address', 'Cases', 'varchar', 255, 0, NULL, '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', NULL, '', '', ''),
+('Casesjjwg_maps_geocode_status_c', 'jjwg_maps_geocode_status_c', 'LBL_JJWG_MAPS_GEOCODE_STATUS', 'Geocode Status', 'Geocode Status', 'Cases', 'varchar', 255, 0, NULL, '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', NULL, '', '', ''),
+('Casesjjwg_maps_lat_c', 'jjwg_maps_lat_c', 'LBL_JJWG_MAPS_LAT', '', 'Latitude', 'Cases', 'float', 10, 0, '0.00000000', '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', '8', '', '', ''),
+('Casesjjwg_maps_lng_c', 'jjwg_maps_lng_c', 'LBL_JJWG_MAPS_LNG', '', 'Longitude', 'Cases', 'float', 11, 0, '0.00000000', '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', '8', '', '', ''),
+('Contactsjjwg_maps_address_c', 'jjwg_maps_address_c', 'LBL_JJWG_MAPS_ADDRESS', 'Address', 'Address', 'Contacts', 'varchar', 255, 0, NULL, '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', NULL, '', '', ''),
+('Contactsjjwg_maps_geocode_status_c', 'jjwg_maps_geocode_status_c', 'LBL_JJWG_MAPS_GEOCODE_STATUS', 'Geocode Status', 'Geocode Status', 'Contacts', 'varchar', 255, 0, NULL, '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', NULL, '', '', ''),
+('Contactsjjwg_maps_lat_c', 'jjwg_maps_lat_c', 'LBL_JJWG_MAPS_LAT', '', 'Latitude', 'Contacts', 'float', 10, 0, '0.00000000', '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', '8', '', '', ''),
+('Contactsjjwg_maps_lng_c', 'jjwg_maps_lng_c', 'LBL_JJWG_MAPS_LNG', '', 'Longitude', 'Contacts', 'float', 11, 0, '0.00000000', '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', '8', '', '', ''),
+('Leadsjjwg_maps_address_c', 'jjwg_maps_address_c', 'LBL_JJWG_MAPS_ADDRESS', 'Address', 'Address', 'Leads', 'varchar', 255, 0, NULL, '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', NULL, '', '', ''),
+('Leadsjjwg_maps_geocode_status_c', 'jjwg_maps_geocode_status_c', 'LBL_JJWG_MAPS_GEOCODE_STATUS', 'Geocode Status', 'Geocode Status', 'Leads', 'varchar', 255, 0, NULL, '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', NULL, '', '', ''),
+('Leadsjjwg_maps_lat_c', 'jjwg_maps_lat_c', 'LBL_JJWG_MAPS_LAT', '', 'Latitude', 'Leads', 'float', 10, 0, '0.00000000', '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', '8', '', '', ''),
+('Leadsjjwg_maps_lng_c', 'jjwg_maps_lng_c', 'LBL_JJWG_MAPS_LNG', '', 'Longitude', 'Leads', 'float', 11, 0, '0.00000000', '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', '8', '', '', ''),
+('Meetingsjjwg_maps_address_c', 'jjwg_maps_address_c', 'LBL_JJWG_MAPS_ADDRESS', 'Address', 'Address', 'Meetings', 'varchar', 255, 0, NULL, '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', NULL, '', '', ''),
+('Meetingsjjwg_maps_geocode_status_c', 'jjwg_maps_geocode_status_c', 'LBL_JJWG_MAPS_GEOCODE_STATUS', 'Geocode Status', 'Geocode Status', 'Meetings', 'varchar', 255, 0, NULL, '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', NULL, '', '', ''),
+('Meetingsjjwg_maps_lat_c', 'jjwg_maps_lat_c', 'LBL_JJWG_MAPS_LAT', '', 'Latitude', 'Meetings', 'float', 10, 0, '0.00000000', '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', '8', '', '', ''),
+('Meetingsjjwg_maps_lng_c', 'jjwg_maps_lng_c', 'LBL_JJWG_MAPS_LNG', '', 'Longitude', 'Meetings', 'float', 11, 0, '0.00000000', '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', '8', '', '', ''),
+('Opportunitiesjjwg_maps_address_c', 'jjwg_maps_address_c', 'LBL_JJWG_MAPS_ADDRESS', 'Address', 'Address', 'Opportunities', 'varchar', 255, 0, NULL, '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', NULL, '', '', ''),
+('Opportunitiesjjwg_maps_geocode_status_c', 'jjwg_maps_geocode_status_c', 'LBL_JJWG_MAPS_GEOCODE_STATUS', 'Geocode Status', 'Geocode Status', 'Opportunities', 'varchar', 255, 0, NULL, '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', NULL, '', '', ''),
+('Opportunitiesjjwg_maps_lat_c', 'jjwg_maps_lat_c', 'LBL_JJWG_MAPS_LAT', '', 'Latitude', 'Opportunities', 'float', 10, 0, '0.00000000', '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', '8', '', '', ''),
+('Opportunitiesjjwg_maps_lng_c', 'jjwg_maps_lng_c', 'LBL_JJWG_MAPS_LNG', '', 'Longitude', 'Opportunities', 'float', 11, 0, '0.00000000', '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', '8', '', '', ''),
+('Opportunitieswfs_opp_capex_c', 'wfs_opp_capex_c', 'LBL_WFS_OPP_CAPEX', '', 'Capital Value', 'Opportunities', 'decimal', 18, 0, '0.00', '2014-10-09 14:14:47', 0, 0, 0, 0, 1, 'true', '2', '', '', ''),
+('Opportunitieswfs_opp_connex_c', 'wfs_opp_connex_c', 'LBL_WFS_OPP_CONNEX', '', '', 'Opportunities', 'decimal', 18, 0, '0.00', '2014-10-09 14:16:20', 0, 0, 0, 0, 1, 'true', '2', '', '', ''),
+('Opportunitieswfs_opp_filtering_c', 'wfs_opp_filtering_c', 'LBL_WFS_OPP_FILTERING', '', '', 'Opportunities', 'decimal', 18, 0, '0.00', '2014-10-09 14:16:59', 0, 0, 0, 0, 1, 'true', '2', '', '', ''),
+('Opportunitieswfs_opp_maintenance_c', 'wfs_opp_maintenance_c', 'LBL_WFS_OPP_MAINTENANCE', NULL, NULL, 'Opportunities', 'decimal', 18, 0, '0.00', '2014-10-09 14:39:34', 0, 0, 0, 0, 1, 'true', '2', NULL, NULL, NULL),
+('Opportunitieswfs_opp_mng_srvc_c', 'wfs_opp_mng_srvc_c', 'LBL_WFS_OPP_MNG_SRVC', '', '', 'Opportunities', 'decimal', 18, 0, '0.00', '2014-10-09 14:15:46', 0, 0, 0, 0, 1, 'true', '2', '', '', ''),
+('Opportunitieswfs_opp_opex_c', 'wfs_opp_opex_c', 'LBL_WFS_OPP_OPEX', 'Calculated field via Logic Hook', 'Operational Value', 'Opportunities', 'decimal', 18, 0, '0.00', '2014-10-09 14:13:36', 0, 0, 0, 0, 1, 'true', '2', '', '', ''),
+('Projectjjwg_maps_address_c', 'jjwg_maps_address_c', 'LBL_JJWG_MAPS_ADDRESS', 'Address', 'Address', 'Project', 'varchar', 255, 0, NULL, '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', NULL, '', '', ''),
+('Projectjjwg_maps_geocode_status_c', 'jjwg_maps_geocode_status_c', 'LBL_JJWG_MAPS_GEOCODE_STATUS', 'Geocode Status', 'Geocode Status', 'Project', 'varchar', 255, 0, NULL, '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', NULL, '', '', ''),
+('Projectjjwg_maps_lat_c', 'jjwg_maps_lat_c', 'LBL_JJWG_MAPS_LAT', '', 'Latitude', 'Project', 'float', 10, 0, '0.00000000', '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', '8', '', '', ''),
+('Projectjjwg_maps_lng_c', 'jjwg_maps_lng_c', 'LBL_JJWG_MAPS_LNG', '', 'Longitude', 'Project', 'float', 11, 0, '0.00000000', '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', '8', '', '', ''),
+('Prospectsjjwg_maps_address_c', 'jjwg_maps_address_c', 'LBL_JJWG_MAPS_ADDRESS', 'Address', 'Address', 'Prospects', 'varchar', 255, 0, NULL, '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', NULL, '', '', ''),
+('Prospectsjjwg_maps_geocode_status_c', 'jjwg_maps_geocode_status_c', 'LBL_JJWG_MAPS_GEOCODE_STATUS', 'Geocode Status', 'Geocode Status', 'Prospects', 'varchar', 255, 0, NULL, '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', NULL, '', '', ''),
+('Prospectsjjwg_maps_lat_c', 'jjwg_maps_lat_c', 'LBL_JJWG_MAPS_LAT', '', 'Latitude', 'Prospects', 'float', 10, 0, '0.00000000', '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', '8', '', '', ''),
+('Prospectsjjwg_maps_lng_c', 'jjwg_maps_lng_c', 'LBL_JJWG_MAPS_LNG', '', 'Longitude', 'Prospects', 'float', 11, 0, '0.00000000', '2014-10-02 11:12:53', 0, 0, 0, 0, 1, 'true', '8', '', '', '');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
